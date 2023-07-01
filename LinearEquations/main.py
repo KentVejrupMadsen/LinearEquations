914fed8062db16d8d6e52a8235b04463818c7606
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-07-01 21:45:09 +0200

    vector class created
    Signed-off-by: Kent Madsen <kent.vejrup.madsen@designermadsen.dk>

--- LinearEquations/main.py
+++ LinearEquations/main.py
@@ -1,7 +1,21 @@
+from vector2d \
+    import Vector2D
 
 
 def main():
-    pass
+    v1 = Vector2D(
+        x=0.4,
+        y=4.5
+    )
+
+    print(str(v1))
+
+    v2 = Vector2D(
+        x=4.4,
+        y=38.2
+    )
+
+    print(str(v2))
 
 
 if __name__ == '__main__':
--- LinearEquations/vector2d.py
+++ LinearEquations/vector2d.py
@@ -7,12 +7,30 @@ class Vector2D:
         self.x: float = x
         self.y: float = y
 
-    def __del__(self):
+    def __del__(
+        self
+    ):
         del             \
             self.x,     \
             self.y
 
-    def get_x(self) -> float:
+    def set_all_to_zero(self):
+        self.set_both(
+            0.0,
+            0.0
+        )
+
+    def set_both(
+        self,
+        x: float,
+        y: float
+    ):
+        self.set_x(x)
+        self.set_y(y)
+
+    def get_x(
+        self
+    ) -> float:
         return self.x
 
     def set_x(
@@ -21,7 +39,9 @@ class Vector2D:
     ) -> None:
         self.x: float = value
 
-    def get_y(self) -> float:
+    def get_y(
+        self
+    ) -> float:
         return self.y
 
     def set_y(
@@ -29,3 +49,52 @@ class Vector2D:
         value: float
     ) -> None:
         self.y: float = value
+
+    def __len__(self) -> int:
+        return len(
+            dir(self)
+        )
+
+    def __getitem__(
+        self,
+        item: str
+    ):
+        return getattr(
+            self,
+            str(item).lower()
+        )
+
+    def __dir__(self) -> list:
+        return [
+            'x',
+            'y'
+        ]
+
+    def __repr__(self) -> str:
+        result_dictionary: dict = dict()
+
+        for entry in dir(self):
+            value = self[entry]
+            result_dictionary[entry] = value
+
+        result: str = str(
+            result_dictionary
+        )
+
+        return result
+
+    def __str__(self):
+        result: str = ''
+
+        end: int = len(self)
+
+        for i in range(end):
+            entry = dir(self)[i]
+            result = result + str(self[entry])
+
+            actual_size: int = i + 1
+            if actual_size < end:
+                result = result + ', '
+
+        final_result: str = '(' + result + ')'
+        return final_result
